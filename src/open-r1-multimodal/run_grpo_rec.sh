a7f3b3b1d826c44a1b5798381ef682c2cc43c537
Yangyi Chen <yangyichen6666@gmail.com>
2025-03-02 11:07:32 -0600

    init the original refcoco pipeline

--- src/open-r1-multimodal/run_grpo_rec.sh
+++ src/open-r1-multimodal/run_grpo_rec.sh
@@ -1,12 +1,12 @@
 cd src/open-r1-multimodal
 
 export DEBUG_MODE="true"
-# export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=3,4,6
 
 RUN_NAME="Qwen2.5-VL-3B-GRPO-REC"
 export LOG_PATH="./debug_log_$RUN_NAME.txt"
 
-torchrun --nproc_per_node="8" \
+torchrun --nproc_per_node="3" \
     --nnodes="1" \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
@@ -16,9 +16,9 @@ torchrun --nproc_per_node="8" \
     --output_dir output/$RUN_NAME \
     --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
     --dataset_name data_config/rec.yaml \
-    --image_root <your_image_root> \
+    --image_root /shared/nas/data/m1/yangyic3/VLM-R1/  \
     --max_prompt_length 1024 \
-    --num_generations 8 \
+    --num_generations 3 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 2 \
     --logging_steps 1 \
@@ -32,3 +32,5 @@ torchrun --nproc_per_node="8" \
     --run_name $RUN_NAME \
     --save_steps 100 \
     --save_only_model true
+
+# original num generiaon = 8
