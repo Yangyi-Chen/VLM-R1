72a3800bb486113f8e8849de3ab1b87ad4514cb8
Yangyi Chen <yangyichen6666@gmail.com>
2025-03-02 16:24:00 -0600

    update grpo for general

---
+++ src/open-r1-multimodal/run_grpo_jsonl.sh
@@ -0,0 +1,37 @@
+cd src/open-r1-multimodal
+
+export DEBUG_MODE="true"
+export CUDA_VISIBLE_DEVICES=3,4,6
+
+RUN_NAME="Qwen2.5-VL-3B-GRPO-jsonl"
+export LOG_PATH="./debug_log_$RUN_NAME.txt"
+
+torchrun --nproc_per_node="3" \
+    --nnodes="1" \
+    --node_rank="0" \
+    --master_addr="127.0.0.1" \
+    --master_port="12346" \
+    src/open_r1/grpo_jsonl.py \
+    --deepspeed local_scripts/zero3.json \
+    --output_dir output/$RUN_NAME \
+    --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
+    --dataset_name JSONL \
+    --data_file_paths /shared/nas/data/m1/yangyic3/VLM-R1/VLM-R1/rec_jsons_processed/refcoco_train.jsonl \
+    --image_folders /shared/nas/data/m1/yangyic3/VLM-R1/VLM-R1  \
+    --max_prompt_length 1024 \
+    --num_generations 3 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 2 \
+    --logging_steps 1 \
+    --bf16 \
+    --torch_dtype bfloat16 \
+    --data_seed 42 \
+    --report_to wandb \
+    --gradient_checkpointing false \
+    --attn_implementation flash_attention_2 \
+    --num_train_epochs 2 \
+    --run_name $RUN_NAME \
+    --save_steps 100 \
+    --save_only_model true
+
+# original num generiaon = 8
