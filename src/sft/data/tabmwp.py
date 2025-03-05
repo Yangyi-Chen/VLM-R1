4419f0ae5fc0e764f7086485aad1337c10008b56
Yangyi-Chen <yangyichen6666@gmail.com>
2025-03-05 13:14:58 -0800

    fix bug_3

--- src/sft/data/tabmwp.py
+++ src/sft/data/tabmwp.py
@@ -18,14 +18,14 @@ from qwen_vl_utils import process_vision_info
 
 class TabMWPModule(BaseDataModule):
     class TabMWPDataset(torch.utils.data.Dataset):
-        def __init__(self, config: dict, processor):
+        def __init__(self, config: dict):
             self.config = config
             self.data_file_path = self.config["data"]["data_file"]
             self.image_folder = self.config["data"]["image_folder"]
             self.data_file = self.read_jsonl(self.data_file_path)
             for item in self.data_file:
                 item['image'] = os.path.join(self.image_folder, item['image'])
-            self.processor = processor
+        
 
     
             
@@ -88,13 +88,11 @@ class TabMWPModule(BaseDataModule):
 
 
 
-    def __init__(self, config: dict, tokenizer):
+    def __init__(self, config: dict, processor):
         super().__init__()
         self.config = config
-        self.tokenizer = tokenizer
-        self.train_dataset = self.TabMWPDataset(
-            config, tokenizer
-        )
+        self.processor = processor
+        self.train_dataset = self.TabMWPDataset(config)
         
         
     
