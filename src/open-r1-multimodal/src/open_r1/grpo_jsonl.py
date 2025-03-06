48550616feb5fc32afdadf45eb035e8d98442ae3
Yangyi-Chen <yangyichen6666@gmail.com>
2025-03-06 13:39:18 -0800

    1

--- src/open-r1-multimodal/src/open_r1/grpo_jsonl.py
+++ src/open-r1-multimodal/src/open_r1/grpo_jsonl.py
@@ -465,4 +465,5 @@ def main(script_args, training_args, model_args):
 if __name__ == "__main__":
     parser = TrlParser((GRPOScriptArguments, GRPOConfig, ModelConfig))
     script_args, training_args, model_args = parser.parse_args_and_config()
+    print(script_args)
     main(script_args, training_args, model_args)
